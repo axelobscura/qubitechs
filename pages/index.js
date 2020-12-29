b9b9250fd968fdeac89bef3249f5d5de8715b863
Axel <axosar@gmail.com>
2020-12-29 14:20:17 -0600

    Texto entrada

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
         <h2 className={styles.titletwo}>QUANTUM INTERNET</h2>
 
         <p className={styles.description}>
-          <small><b>Quantum computing differs from ordinary computing in many ways.</b><br/>First, ordinary computers are built around the concept of the bit, which is effectively a switch that can be flipped on or off -- what computer professionals call a 1 or 0.<br/><b>In contrast, QUANTUM COMPUTERS use the QUBIT, short for "quantum bit."</b></small>
+          <small><b>Quantum computing differs from ordinary computing in many ways</b><br/>First, ordinary computers are built around the concept of the bit, which is effectively a switch that can be flipped on or off -- what computer professionals call a 1 or 0.<br/><b>In contrast, QUANTUM COMPUTERS use the QUBIT, short for "quantum bit"</b></small>
         </p>
 
       </main>
